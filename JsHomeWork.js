06127b4787ce1ef0f21786c80623eed55b4c9347
IvanRStoyanov <32427929+IvanRStoyanov@users.noreply.github.com>
2018-02-17 17:44:26 +0200

    final function
    with implemented option for choosing number of ReRolls

--- JsHomeWork.js
+++ JsHomeWork.js
@@ -10,8 +10,8 @@ function roll() {
 
     //setting AI dices
     for (let i = 0; i < 5; i++) {
-        let currentDice = Math.floor((Math.random() * 6) + 1);
-        dicesAI.push(currentDice);
+        let AiDice = Math.floor((Math.random() * 6) + 1);
+        dicesAI.push(AiDice);
     }
 
     //showing player dices
@@ -21,26 +21,70 @@ function roll() {
 
     //showing AI dices
     for (let i = 1; i <= dicesPlayer.length; i++) {
-        document.getElementById('_$' + i).innerHTML = dicesPlayer[i-1];
+        document.getElementById('_$' + i).innerHTML = dicesAI[i-1];
     }
+
     document.getElementById('roll').setAttribute('disabled', 'disabled');
     setClickable();
+
+    //this is optional for making different reRolls
+    let reRolls = document.getElementById('reRollsQuantity').value;
+    if(reRolls <= 0) document.getElementById('reRoll').setAttribute('disabled', 'disabled');
+    document.getElementById('reRollsQuantity').setAttribute('disabled', 'disabled');
 }
 
 //making dices clickable
 function setClickable() {
-    for (let i = 1; i <= dices.length; i++) {
-        document.getElementById('_' + i).addEventListener('click', lockDice());
+    for(let i = 1; i <= dicesPlayer.length; i++) {
+        document.getElementById('_' + i).addEventListener('click', forRollDice);
     }
 }
 
-function lockDice() {
-    for (let i = 1; i <= dices.length; i++) {
-        let currentElement = document.getElementById('_' + i);
-        if(currentElement.classList.contains('locked')){
-            currentElement.classList.remove('locked');
-        }else {
-            currentElement.classList.add('locked');
+//mark dices
+function forRollDice() {
+    if(this.classList.contains('forRoll')){
+        this.classList.remove('forRoll');
+    }else {
+        this.classList.add('forRoll');
+    }
+}
+
+function reRoll() {
+    //this is optional for making different reRolls
+    let reRolls = document.getElementById('reRollsQuantity').value;
+    if(reRolls > 0) {
+        let reRolledDicesPosition = [];
+        for (let i = 1; i <= 5; i++) {
+            //getting markedDice
+            if (document.getElementById('_' + i).classList.contains('forRoll')) {
+                reRolledDicesPosition.push(i);
+            }
+        }
+
+        if (reRolledDicesPosition.length > 0) {
+            //new rolls
+            let newRoll = [];
+            for (let i = 0; i < reRolledDicesPosition.length; i++) {
+                let newDiceRoll = Math.floor((Math.random() * 6) + 1);
+                newRoll.push(newDiceRoll);
+            }
+
+            //replacing marked dices with new rolls
+            for (let i = 0; i < reRolledDicesPosition.length; i++) {
+                dicesPlayer.splice(reRolledDicesPosition[i] - 1, 1, newRoll[i]);
+            }
+
+            //showing new rolls - should be replaced with Tisho's function
+            for (let i = 1; i <= dicesPlayer.length; i++) {
+                document.getElementById('_' + i).innerHTML = dicesPlayer[i - 1];
+                document.getElementById('_' + i).classList.remove('forRoll');
+            }
+
         }
     }
+    --reRolls;
+    if(reRolls === 0) {
+        document.getElementById('reRoll').setAttribute('disabled', 'disabled');
+    }
+    document.getElementById('reRollsQuantity').value = reRolls;
 }
